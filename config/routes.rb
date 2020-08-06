40954db930a1fd2add05dc3012b4449f5fc58c69
HRK-KY <humansequencer0@gmail.com>
2020-08-06 14:51:30 +0900

    set routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "tops#index"
+
+  resources :answers ,only: [ :index ]
+  resources :themes ,only: [ :index, :new, :create ]
 end
