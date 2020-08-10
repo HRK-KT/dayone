e056d30e3b926753f534d47db97cdad2f5c99468
HRK-KY <humansequencer0@gmail.com>
2020-08-10 09:59:01 +0900

    edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "tops#index"
 
-  resources :answers ,only: [ :index, :new, :create ]
+  resources :answers ,only: [ :index, :new, :create, :show ]
   resources :themes ,only: [ :index, :new, :create ]
 end
