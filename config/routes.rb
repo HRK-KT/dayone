0f0e597523be2998cf46db5c3d6a56105ff594df
HRK-KY <humansequencer0@gmail.com>
2020-08-12 09:59:26 +0900

    add user show page as mypage

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "tops#index"
 
-  resources :users ,only: [:edit, :update]
+  resources :users ,only: [:edit, :update, :show]
   resources :answers ,only: [ :index, :new, :create, :show ]
     
   resources :themes ,only: [ :index, :new, :create ] do
