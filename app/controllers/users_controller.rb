f989fd066be30c33073d5347180be09585df30dc
HRK-KY <humansequencer0@gmail.com>
2020-08-12 09:20:45 +0900

    add user edit  function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,21 @@ class UsersController < ApplicationController
   def create
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:name, :email, :sex, :age, :region, :job)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "tops#index"
 
+  resources :users ,only: [:edit, :update]
   resources :answers ,only: [ :index, :new, :create, :show ]
     
   resources :themes ,only: [ :index, :new, :create ] do
