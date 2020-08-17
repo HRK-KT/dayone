1dc2e4fe97478a92dc299380440967780d005412
HRK-KY <humansequencer0@gmail.com>
2020-08-17 11:56:15 +0900

    add user profileimage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,7 @@ class UsersController < ApplicationController
   private 
 
   def user_params
-    params.require(:user).permit(:name, :email, :sex, :age, :region, :job)
+    params.require(:user).permit(:name, :email, :sex, :age, :region, :job, :image )
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  mount_uploader :image, UserUploader
+
   has_many :themes
   has_many :answers
 end
